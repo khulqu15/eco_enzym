96dfbb18ebdbbeeedb16c2492a8489abc2e8716a
= <=>
2023-04-18 23:45:04 +0700

    fixing list

---
+++ .history/src/firebaseConfig_20230418230944.ts
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyC-mHNOM1cUsizZqeMH9YzmyiKAljTO6pI",
+    authDomain: "ecoenzym-d56ec.firebaseapp.com",
+    databaseURL: "https://ecoenzym-d56ec-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ecoenzym-d56ec",
+    storageBucket: "ecoenzym-d56ec.appspot.com",
+    messagingSenderId: "101999972533",
+    appId: "1:101999972533:web:5e70ee657ffbfdc93b2efc",
+  };
+  
